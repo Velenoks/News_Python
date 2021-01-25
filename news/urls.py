f6af5c646f2479bb0d973e9062f97003e41f6c46
Velenoks <pavel.zakharov.1994@yandex.ru>
2021-01-25 21:40:12 +0400

    add urls

---
+++ news/urls.py
@@ -0,0 +1,22 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from . import views
+
+
+app_name = 'news'
+
+router = DefaultRouter()
+router.register(r'categories',
+                views.CategoryViewSet,
+                basename=app_name)
+router.register(r'news',
+                views.TitleViewSet,
+                basename=app_name)
+router.register(r'news/(?P<title_id>\d+)/comments',
+                views.ReviewViewSet,
+                basename=app_name)
+
+urlpatterns = [
+    path('v1/', include(router.urls)),
+]
